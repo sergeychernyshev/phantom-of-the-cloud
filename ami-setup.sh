7c3198ea5e24dd7413ebfbd45c741c0159953105
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2014-05-24 05:03:25 +0000

    Renamed motd file

--- ami-setup.sh
+++ ami-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /etc/update-motd.d/; sudo rm -f 30-banner; sudo ln -s /home/ec2-user/motd.head 30-banner
+cd /etc/update-motd.d/; sudo rm -f 30-banner; sudo ln -s /home/ec2-user/motd.banner 30-banner
 sudo /usr/sbin/update-motd
 
 cd /etc/init.d; sudo rm -f boot.sh; sudo ln -s /home/ec2-user/boot.sh
