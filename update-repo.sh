6ee349e97241a0935dc630a400499a6a29b00375
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2014-06-11 17:41:19 -0400

    Added tarball support

--- update-repo.sh
+++ update-repo.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 REPO=`/usr/bin/curl -s http://169.254.169.254/latest/user-data | grep 'git-repo:' | sed -e 's/git-repo: //'`
-AWS_KEY=`/usr/bin/curl -s http://169.254.169.254/latest/user-data | grep 'aws-key:' | sed -e 's/aws-key: //'`
-AWS_SECRET=`/usr/bin/curl -s http://169.254.169.254/latest/user-data | grep 'aws-secret:' | sed -e 's/aws-secret: //'`
+TARBALL=`/usr/bin/curl -s http://169.254.169.254/latest/user-data | grep 'tarball-repo:' | sed -e 's/tarball-repo: //'`
 
-echo "[aws]" >/home/ec2-user/system.ini
-echo "aws-key = $AWS_KEY" >>/home/ec2-user/system.ini
-echo "aws-secret = $AWS_SECRET" >>/home/ec2-user/system.ini
-
-echo "[repo]" >>/home/ec2-user/system.ini
-echo "git-repo = $REPO" >>/home/ec2-user/system.ini
-
-if [ -d /home/ec2-user/user-repo ]; then
-	cd /home/ec2-user/user-repo
-	/usr/bin/git pull 
+if [ "x$REPO" -ne "x" ]; then
+	if [ -d /home/ec2-user/user-repo ]; then
+		cd /home/ec2-user/user-repo
+		/usr/bin/git pull 
+	else
+		/usr/bin/git clone $REPO /home/ec2-user/user-repo
+	fi
 else
-	/usr/bin/git clone $REPO /home/ec2-user/user-repo
+	rm -rf /home/ec2-user/user-repo
+	mkdir /home/ec2-user/user-repo
+	cd /home/ec2-user/user-repo
+	curl -s $TARBALL | tar -xz
 fi
