d07c63b599e5a3c9a8bba1e3b87c8d98642eb374
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2015-06-06 15:26:56 +0000

    Some comments to help identify what the hell is it

--- ami-setup.sh
+++ ami-setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This is an attempt to automate setting up AMI from files in ec2-user's home folder.
+# It probably requires much more work, especially with all the packages installed and all.
+# This should be moved over to an ansible/puppet/chef configuration so it can leave outside AMI itself
+
 cd /etc/update-motd.d/; sudo rm -f 30-banner; sudo ln -s /home/ec2-user/motd.banner 30-banner
 sudo /usr/sbin/update-motd
 
