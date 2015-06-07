3077e91c23cd741987a1f5b08d3776a4ee02e5ad
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2015-06-07 05:14:45 +0000

    Only create credentials info if it is specified to be compatible with IAM roles

--- boot.sh
+++ boot.sh
@@ -15,22 +15,29 @@ AWS_KEY=`/usr/bin/curl -s http://169.254.169.254/latest/user-data | grep 'aws-ke
 AWS_SECRET=`/usr/bin/curl -s http://169.254.169.254/latest/user-data | grep 'aws-secret:' | sed -e 's/aws-secret: //'`
 AWS_REGION=`/usr/bin/curl -s http://169.254.169.254/latest/user-data | grep 'aws-region:' | sed -e 's/aws-region: //'`
 
-echo "[aws]" >/home/ec2-user/system.ini
-echo "aws-key = $AWS_KEY" >>/home/ec2-user/system.ini
-echo "aws-secret = $AWS_SECRET" >>/home/ec2-user/system.ini
-echo "aws-region = $AWS_REGION" >>/home/ec2-user/system.ini
+# create system.ini
+echo "" >/home/ec2-user/system.ini
+
+# populate fiels with AWS credentials if set in user data (otherwise relies on IAM roles that work on library level)
+if [ -z "$AWS_KEY" ]; then
+	echo "[aws]" >>/home/ec2-user/system.ini
+	echo "aws-key = $AWS_KEY" >>/home/ec2-user/system.ini
+	echo "aws-secret = $AWS_SECRET" >>/home/ec2-user/system.ini
+	echo "aws-region = $AWS_REGION" >>/home/ec2-user/system.ini
+
+	# More or less standard credentials file
+	mkdir -p /home/ec2-user/.aws
+	echo "[default]" >/home/ec2-user/.aws/credentials
+	echo "region = $AWS_REGION" >>/home/ec2-user/.aws/credentials
+	echo "aws_access_key_id = $AWS_KEY" >>/home/ec2-user/.aws/credentials
+	echo "aws_secret_access_key = $AWS_SECRET" >>/home/ec2-user/.aws/credentials
+	chown ec2-user.ec2-user -R /home/ec2-user/.aws
+fi
+
 echo "" >>/home/ec2-user/system.ini
 echo "[repo]" >>/home/ec2-user/system.ini
 echo "git-repo = $REPO" >>/home/ec2-user/system.ini
 echo "tarball-repo = $TARBALL" >>/home/ec2-user/system.ini
-
-# More or less standard credentials file
-mkdir -p /home/ec2-user/.aws
-echo "[default]" >/home/ec2-user/.aws/credentials
-echo "region = $AWS_REGION" >>/home/ec2-user/.aws/credentials
-echo "aws_access_key_id = $AWS_KEY" >>/home/ec2-user/.aws/credentials
-echo "aws_secret_access_key = $AWS_SECRET" >>/home/ec2-user/.aws/credentials
-chown ec2-user.ec2-user -R /home/ec2-user/.aws
 
 # Getting the code from the repo provided in user data's git-repo attribute
 su -l ec2-user -c /home/ec2-user/update-repo.sh 2>>/tmp/boot.err >>/tmp/boot.log
